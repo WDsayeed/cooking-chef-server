5517b087581d00128abcf2434ac1a84fd3943e0c
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-02 09:55:38 +0600

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+const chef = require('./data/chef.json')
 app.get('/', (req, res)=>{
         res.send('cooking is running')
+})
+
+app.get('/chef', (req, res)=>{
+        res.send(chef)
 })
 
 app.listen(port, ()=>{
